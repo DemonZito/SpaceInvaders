ecba48a83671067db53d7484ee32a407325929e4
Jack Mair <jwjmair@gmail.com>
2017-06-09 16:14:26 +1200

    added a nonfunctional debug menu

--- Space Invaders/Space Invaders/main.cpp
+++ Space Invaders/Space Invaders/main.cpp
@@ -23,10 +23,12 @@
 #include "utils.h"
 #include "level.h"
 #include "player.h"
+#include "resource.h"
 
 const int kiWidth = 960;
 const int kiHeight = 720;
 
+HWND g_hDlgDebug;
 
 #define WINDOW_CLASS_NAME L"BSENGGFRAMEWORK"
 
@@ -35,6 +37,21 @@ WindowProc(HWND _hWnd, UINT _uiMsg, WPARAM _wParam, LPARAM _lParam)
 {
 	switch (_uiMsg)
 	{
+	case WM_KEYDOWN:
+	{
+		switch (_wParam)
+		{
+		case 0x1B:
+		{
+			ShowWindow(g_hDlgDebug, SW_SHOWNORMAL);
+			break;
+		}
+		default:
+			break;
+		}
+		return (0);
+	}
+		break;
 	case WM_MOUSEMOVE:
 	{
 		int iMouseX = LOWORD(_lParam);
@@ -58,6 +75,106 @@ WindowProc(HWND _hWnd, UINT _uiMsg, WPARAM _wParam, LPARAM _lParam)
 	}
 
 	return (DefWindowProc(_hWnd, _uiMsg, _wParam, _lParam));
+}
+
+BOOL CALLBACK DebugDlgProc(HWND _hwnd,
+	UINT _msg,
+	WPARAM _wparam,
+	LPARAM _lparam)
+	{
+		static float _value;
+		switch (_msg)
+		{
+		case WM_COMMAND:
+		{
+			switch (LOWORD(_wparam))
+			{
+			case IDC_RADIO13:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO13, IDC_RADIO15, IDC_RADIO13);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO14:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO13, IDC_RADIO15, IDC_RADIO14);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO15:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO13, IDC_RADIO15, IDC_RADIO15);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO4:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO4, IDC_RADIO6, IDC_RADIO4);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO5:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO4, IDC_RADIO6, IDC_RADIO5);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO6:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO4, IDC_RADIO6, IDC_RADIO6);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO7:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO7, IDC_RADIO9, IDC_RADIO7);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO8:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO7, IDC_RADIO9, IDC_RADIO8);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO9:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO7, IDC_RADIO9, IDC_RADIO9);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO10:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO10, IDC_RADIO12, IDC_RADIO10);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO11:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO10, IDC_RADIO12, IDC_RADIO11);
+				return TRUE;
+				break;
+			}
+			case IDC_RADIO12:
+			{
+				CheckRadioButton(_hwnd, IDC_RADIO10, IDC_RADIO12, IDC_RADIO12);
+				return TRUE;
+				break;
+			}
+			case WM_CLOSE:
+			{
+				ShowWindow(_hwnd, SW_HIDE);
+				return TRUE;
+				break;
+			}
+			default:
+				break;
+			}
+		}
+		default:
+			break;
+		}
+	return FALSE;
 }
 
 HWND
@@ -101,6 +218,8 @@ CreateAndRegisterWindow(HINSTANCE _hInstance, int _iWidth, int _iHeight, const w
 		// Failed to create.
 		return (0);
 	}
+
+	g_hDlgDebug = CreateDialog(_hInstance, MAKEINTRESOURCE(IDD_DIALOG1), hwnd, DebugDlgProc);
 
 	return (hwnd);
 }
